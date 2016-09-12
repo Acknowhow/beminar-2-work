90c4878d17aeed32f07e66d82754c82d254a6d25
Vadim Korabelnik <vad@oblako9.ru>
2016-09-12 23:46:49 +0300

    Added button and form js

---
+++ common.blocks/button/button.js
@@ -0,0 +1,20 @@
+
+// We need to enhance the code in order to make the button independent
+
+/** $('.button').on('click', function(){
+
+    e.preventDefault();
+
+    if (!/\S+@\S+\.\S+/.test($('.form__input_type_email').val())) {
+
+        $('.form__popup').addClass('form__popup_visible');
+
+    } else {
+
+        $('.form__popup').removeClass('form__popup_visible');
+
+    }
+
+});
+
+ */
